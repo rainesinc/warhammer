0fba31b47cdcd905e014339818aac9739f4c47fd
Ryan Raines <rraines22@proton.me>
2024-01-29 18:23:39 -0500

    Mockito service test

---
+++ src/test/java/com/rainesinc/warhammer/service/FactionServiceMockTest.java
@@ -0,0 +1,46 @@
+package com.rainesinc.warhammer.service;
+
+import com.rainesinc.warhammer.entity.Faction;
+import com.rainesinc.warhammer.entity.Miniature;
+import com.rainesinc.warhammer.repository.FactionRepository;
+import com.rainesinc.warhammer.repository.MiniatureRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class FactionServiceMockTest {
+    @Mock
+    FactionRepository factionRepository;
+
+    @InjectMocks
+    private FactionService factionService;
+
+
+    @Test
+    public void canAddMiniature(){
+        // given
+        Faction faction = new Faction();
+        faction.setName("BRETONNIA");
+
+        // when
+        factionService.addFaction(faction);
+
+        // then
+        ArgumentCaptor<Faction> factionArgumentCaptor = ArgumentCaptor.forClass(
+                Faction.class
+        );
+        verify(factionRepository).save(factionArgumentCaptor.capture());
+        Faction capturedFaction = factionArgumentCaptor.getValue();
+
+        assertThat(capturedFaction).isEqualTo(faction);
+
+    }
+
+}
